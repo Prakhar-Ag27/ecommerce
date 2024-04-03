4751b82d3deb36c30ffa732dd6b1e8b34850c161
Prakhar-Ag27 <agrawal.prakhar35@gmail.com>
2024-04-03 09:36:34 +0530

    modified styling

--- ecommerce/src/ecommerce/UserPage.java
+++ ecommerce/src/ecommerce/UserPage.java
@@ -1,22 +1,8 @@
 package ecommerce;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
-import javax.swing.JTextField;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class UserPage implements ActionListener {
 	maxFrame m = new maxFrame("User HomePage - ECommerce App");
@@ -26,39 +12,51 @@ public class UserPage implements ActionListener {
 
 	UserPage() {
 		JPanel nav = new JPanel(); // Panel for nav
-		nav.setBackground(Color.DARK_GRAY);
+		nav.setBackground(Color.WHITE);
 		nav.setPreferredSize(new Dimension(100, 100));
+		nav.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10)); // Add spacing between components
 
 		JPanel search = new JPanel();
-		search.setBackground(Color.DARK_GRAY);
+		search.setBackground(Color.WHITE);
 		searchButton = new JButton("Search");
 		JTextField searchBox = new JTextField();
-		searchButton.setFocusable(false);
-		searchBox.setPreferredSize(new Dimension(420, 50));
-		searchButton.setPreferredSize(new Dimension(150, 50));
+		searchBox.setPreferredSize(new Dimension(300, 30)); // Adjust search box size
+		searchButton.setPreferredSize(new Dimension(100, 30)); // Adjust search button size
 		searchButton.addActionListener(this);
 		search.add(searchBox);
 		search.add(searchButton);
-		
+
 		ImageIcon walletIcon = new ImageIcon("src/img/wallet.png");
 		JButton walletButton = new JButton();
 		walletButton.setText("Wallet");
-		Image scaledImage = walletIcon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
-        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+		Image scaledImage = walletIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+		ImageIcon scaledIcon = new ImageIcon(scaledImage);
 		walletButton.setIcon(scaledIcon);
 		walletButton.setHorizontalTextPosition(JButton.RIGHT);
 		walletButton.setVerticalTextPosition(JButton.CENTER);
 		walletButton.setFocusable(false);
+		walletButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Clicked Wallet");
+			}
+		});
+
+		ImageIcon originalIcon = new ImageIcon("src/img/logo.jpg");
+		Image scaledAppImage = originalIcon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+		ImageIcon scaledAppIcon = new ImageIcon(scaledAppImage);
+		JLabel app = new JLabel();
+		app.setIcon(scaledAppIcon);
 
 		userAvatarButton = new JButton("Username");
-		userAvatarButton.setPreferredSize(new Dimension(250, 50));
+		userAvatarButton.setPreferredSize(new Dimension(150, 30)); // Adjust userAvatarButton size
 		userAvatarButton.addActionListener(this);
 		userAvatarButton.setFocusable(false);
 
 		JPanel screen = new JPanel(); // Panel for main screen
-		// screen.setBackground(Color.blue);
+		screen.setBackground(Color.LIGHT_GRAY); // Set screen background color
 
-		nav.setLayout(new FlowLayout(FlowLayout.CENTER, 100, 0));
+		nav.add(app);
 		nav.add(search);
 		nav.add(walletButton);
 		nav.add(userAvatarButton);
@@ -69,32 +67,30 @@ public class UserPage implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == searchButton) {
-			System.out.println("Search Query inititated.");
+			System.out.println("Search Query initiated.");
 		}
 		if (e.getSource() == userAvatarButton) {
 			JPopupMenu popupMenu = new JPopupMenu();
-			popupMenu.setPreferredSize(new Dimension(250, 100));
+			popupMenu.setPreferredSize(new Dimension(150, 100));
 			JMenuItem profileItem = new JMenuItem("View Profile");
 			JMenuItem shoppingCart = new JMenuItem("Shopping Cart");
 			JMenuItem logoutItem = new JMenuItem("Logout");
-            logoutItem.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    // Perform logout action here
-                	m.dispose();
-                	new LoginPage();
-                    JOptionPane.showMessageDialog(null, "Logged out!");
-                    // Close the dropdown menu
-                    popupMenu.setVisible(false);
-                    
-                    
-                }
-            });
-            
-            popupMenu.add(profileItem);
-            popupMenu.add(shoppingCart);
-            popupMenu.add(logoutItem);
-            popupMenu.show(userAvatarButton, 0, userAvatarButton.getHeight());
+			logoutItem.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// Perform logout action here
+					m.dispose();
+					new LoginPage();
+					JOptionPane.showMessageDialog(null, "Logged out!");
+					// Close the dropdown menu
+					popupMenu.setVisible(false);
+				}
+			});
+
+			popupMenu.add(profileItem);
+			popupMenu.add(shoppingCart);
+			popupMenu.add(logoutItem);
+			popupMenu.show(userAvatarButton, 0, userAvatarButton.getHeight());
 		}
 	}
 }
