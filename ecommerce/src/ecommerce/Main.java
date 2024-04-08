539989aea10597e78dd78bd1cace25b91d5cb938
Prakhar-Ag27 <agrawal.prakhar35@gmail.com>
2024-04-08 15:51:51 +0530

    .

--- ecommerce/src/ecommerce/Main.java
+++ ecommerce/src/ecommerce/Main.java
@@ -1,14 +1,13 @@
 package ecommerce;
 
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-LoginPage l = new LoginPage();
+		LoginPage l = new LoginPage();
 //		new CheckoutPage();
-		//new wallet();
-		
+		// new wallet();
+
 	}
 
 }
--- ecommerce/src/ecommerce/UserPage.java
+++ ecommerce/src/ecommerce/UserPage.java
@@ -9,108 +9,132 @@ public class UserPage implements ActionListener {
 	JLabel l = new JLabel("Welcome to User Page!");
 	JButton searchButton;
 	JButton userAvatarButton;
+	JPanel screen;
 
 	UserPage() {
-        JPanel nav = new JPanel(); // Panel for nav
-        nav.setBackground(Color.WHITE);
-        nav.setPreferredSize(new Dimension(100, 100));
-        nav.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10)); // Add spacing between components
+		JPanel nav = new JPanel(); // Panel for nav
+		nav.setBackground(Color.WHITE);
+		nav.setPreferredSize(new Dimension(100, 100));
+		nav.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10)); // Add spacing between components
 
-        JPanel search = new JPanel();
-        search.setBackground(Color.WHITE);
-        searchButton = new JButton("Search");
-        JTextField searchBox = new JTextField();
-        searchBox.setPreferredSize(new Dimension(300, 40)); // Adjust search box size
-        searchButton.setPreferredSize(new Dimension(100, 40)); // Adjust search button size
-        searchButton.setFocusable(false);
-        searchButton.addActionListener(this);
-        search.add(searchBox);
-        search.add(searchButton);
+		JPanel search = new JPanel();
+		search.setBackground(Color.WHITE);
+		searchButton = new JButton("Search");
+		JTextField searchBox = new JTextField();
+		searchBox.setPreferredSize(new Dimension(300, 40)); // Adjust search box size
+		searchButton.setPreferredSize(new Dimension(100, 40)); // Adjust search button size
+		searchButton.setFocusable(false);
+		searchButton.addActionListener(this);
+		search.add(searchBox);
+		search.add(searchButton);
 
-        ImageIcon walletIcon = new ImageIcon("src/img/wallet.png");
-        JButton walletButton = new JButton();
-        walletButton.setText("Wallet");
-        Image scaledImage = walletIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
-        ImageIcon scaledIcon = new ImageIcon(scaledImage);
-        walletButton.setIcon(scaledIcon);
-        walletButton.setHorizontalTextPosition(JButton.RIGHT);
-        walletButton.setVerticalTextPosition(JButton.CENTER);
-        walletButton.setFocusable(false);
-        walletButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new wallet();
-            }
-        });
+		ImageIcon walletIcon = new ImageIcon("src/img/wallet.png");
+		JButton walletButton = new JButton();
+		walletButton.setText("Wallet");
+		Image scaledImage = walletIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+		ImageIcon scaledIcon = new ImageIcon(scaledImage);
+		walletButton.setIcon(scaledIcon);
+		walletButton.setHorizontalTextPosition(JButton.RIGHT);
+		walletButton.setVerticalTextPosition(JButton.CENTER);
+		walletButton.setFocusable(false);
+		walletButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new wallet();
+			}
+		});
 
-        ImageIcon shoppingCartImg = new ImageIcon("src/img/shoppingCart.png");
-        JButton shoppingCart = new JButton();
-        shoppingCart.setText("Shopping Cart");
-        Image scaledCartImage = shoppingCartImg.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
-        ImageIcon scaledCartIcon = new ImageIcon(scaledCartImage);
-        shoppingCart.setIcon(scaledCartIcon);
-        shoppingCart.setHorizontalTextPosition(JButton.RIGHT);
-        shoppingCart.setVerticalTextPosition(JButton.CENTER);
-        shoppingCart.setFocusable(false);
-        shoppingCart.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new CheckoutPage();
-            }
-        });
+		ImageIcon shoppingCartImg = new ImageIcon("src/img/shoppingCart.png");
+		JButton shoppingCart = new JButton();
+		shoppingCart.setText("Shopping Cart");
+		Image scaledCartImage = shoppingCartImg.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+		ImageIcon scaledCartIcon = new ImageIcon(scaledCartImage);
+		shoppingCart.setIcon(scaledCartIcon);
+		shoppingCart.setHorizontalTextPosition(JButton.RIGHT);
+		shoppingCart.setVerticalTextPosition(JButton.CENTER);
+		shoppingCart.setFocusable(false);
+		shoppingCart.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new CheckoutPage();
+			}
+		});
+		
+		ImageIcon advSearchImgIcon = new ImageIcon("src/img/search.png");
+		JButton advSearchButton = new JButton();
+		advSearchButton.setText("Advanced Search");
+		Image advSearchImg = advSearchImgIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+		advSearchImgIcon = new ImageIcon(advSearchImg);
+		advSearchButton.setIcon(advSearchImgIcon);
+		advSearchButton.setHorizontalTextPosition(JButton.RIGHT);
+		advSearchButton.setVerticalTextPosition(JButton.CENTER);
+		advSearchButton.setFocusable(false);
+		advSearchButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new AdvancedSearch();
+			}
+		});
 
-        ImageIcon originalIcon = new ImageIcon("src/img/logo.jpg");
-        Image scaledAppImage = originalIcon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
-        ImageIcon scaledAppIcon = new ImageIcon(scaledAppImage);
-        JLabel app = new JLabel();
-        app.setIcon(scaledAppIcon);
+		ImageIcon originalIcon = new ImageIcon("src/img/logo.jpg");
+		Image scaledAppImage = originalIcon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+		ImageIcon scaledAppIcon = new ImageIcon(scaledAppImage);
+		JLabel app = new JLabel();
+		app.setIcon(scaledAppIcon);
 
-        userAvatarButton = new JButton("Username");
-        userAvatarButton.setPreferredSize(new Dimension(150, 40)); // Adjust userAvatarButton size
-        userAvatarButton.addActionListener(this);
-        userAvatarButton.setFocusable(false);
+		userAvatarButton = new JButton("Username");
+		userAvatarButton.setPreferredSize(new Dimension(150, 40)); // Adjust userAvatarButton size
+		userAvatarButton.addActionListener(this);
+		userAvatarButton.setFocusable(false);
 
-        JPanel screen = new JPanel(); // Panel for main screen
-        screen.setBackground(Color.LIGHT_GRAY); // Set screen background color
+		screen = new JPanel(); // Panel for main screen
+		screen.setBackground(Color.LIGHT_GRAY); // Set screen background color
 
-        // Change layout of screen panel to BoxLayout with Y_AXIS alignment
-        screen.setLayout(new BoxLayout(screen, BoxLayout.Y_AXIS));
+		// Change layout of screen panel to BoxLayout with Y_AXIS alignment
+		screen.setLayout(new BoxLayout(screen, BoxLayout.Y_AXIS));
 
-        // Add screen panel to JScrollPane
-        JScrollPane scrollPane = new JScrollPane(screen, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-         // Set preferred size of JScrollPane
+		// Add screen panel to JScrollPane
+		JScrollPane scrollPane = new JScrollPane(screen, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		// Set preferred size of JScrollPane
 
-        nav.add(app);
-        nav.add(search);
-        nav.add(walletButton);
-        nav.add(shoppingCart);
-        nav.add(userAvatarButton);
+		nav.add(app);
+		nav.add(search);
+		nav.add(advSearchButton);
+		nav.add(walletButton);
+		nav.add(shoppingCart);
+		nav.add(userAvatarButton);
 
-        m.add(nav, BorderLayout.NORTH);
-        m.add(scrollPane, BorderLayout.CENTER);
+		m.add(nav, BorderLayout.NORTH);
+		m.add(scrollPane, BorderLayout.CENTER);
 
-        // Create a panel for each row
-        for (int i = 0; i < 100; i += 6) {
-            JPanel rowPanel = new JPanel(); // Panel for each row
-            rowPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 57, 25)); // Set layout for row panel
+		// Create a panel for each row
+		for (int i = 0; i < 100; i += 6) {
+			JPanel rowPanel = new JPanel(); // Panel for each row
+			rowPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 57, 25)); // Set layout for row panel
 
-            // Add six ProductCard components to each row
-            for (int j = i; j < i + 6; j++) {
-                if (j < 7) { // Ensure not to add more than 100 ProductCard components
-                    rowPanel.add(new ProductCard("name", "description", 23.99));
-                }
-            }
+			// Add six ProductCard components to each row
+			for (int j = i; j < i + 6; j++) {
+				if (j < 7) { // Ensure not to add more than 100 ProductCard components
+					rowPanel.add(new ProductCard("name", "description", 23.99));
+				}
+			}
 
-            // Add row panel to the screen panel
-            screen.add(rowPanel);
-        }
+			// Add row panel to the screen panel
+			screen.add(rowPanel);
+		}
 
-        m.setVisible(true);
-    }
+		m.setVisible(true);
+	}
 
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == searchButton) {
-			System.out.println("Search Query initiated.");
+			screen.removeAll();
+
+			// JDBC here for adding content according to search query;
+
+			screen.revalidate();
+			screen.repaint();
 		}
 		if (e.getSource() == userAvatarButton) {
 			JPopupMenu popupMenu = new JPopupMenu();
