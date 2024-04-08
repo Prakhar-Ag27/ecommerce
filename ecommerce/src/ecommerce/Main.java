120e610f44534ebfb17f9f124b067a939175c6a3
dhruvbhandari234 <f20210925@pilani.bits-pilani.ac.in>
2024-04-08 18:12:25 +0530

    User page with double click edit disabled

--- ecommerce/src/ecommerce/Main.java
+++ ecommerce/src/ecommerce/Main.java
@@ -4,17 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	LoginPage l = new LoginPage();
+	//LoginPage l = new LoginPage();
 //		new CheckoutPage();
 
 		// new wallet();
 
 //		new wallet();
 //		new Cart().setVisible(true);
-		//new SellerPage();
+		new SellerPage();
 
 	//	new Cart().setVisible(true);
-//		new User();
+		new User();
 
 	}
 
--- ecommerce/src/ecommerce/User.java
+++ ecommerce/src/ecommerce/User.java
@@ -20,7 +20,12 @@ public class User extends JFrame {
 
         // Create a table model with columns
         String[] columns = {"Field Name", "Data"};
-        model = new DefaultTableModel(columns, 0);
+        model = new DefaultTableModel(columns, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false; // Disable editing by default
+            }
+        };
         table = new JTable(model);
 
         // Add user data to the table
@@ -35,19 +40,18 @@ public class User extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int selectedRow = table.getSelectedRow();
-                if(selectedRow == 3){
-                    new wallet();
+                if (selectedRow == 3) {
+                	new wallet();
                 }
-                 else if (selectedRow != -1) {
-                	System.out.println(selectedRow);
-                     // Open a menu or dialog to edit the selected user's information
-                     String fieldName = (String) model.getValueAt(selectedRow, 0);
-                     String currentData = (String) model.getValueAt(selectedRow, 1);
-                     String newData = JOptionPane.showInputDialog(null, "Enter new data for " + fieldName + ":", currentData);
-                     if (newData != null) {
-                         model.setValueAt(newData, selectedRow, 1);
-                     }
-                 }else {
+                else if (selectedRow != -1) {
+                    // Open a menu or dialog to edit the selected user's information
+                    String fieldName = (String) model.getValueAt(selectedRow, 0);
+                    String currentData = (String) model.getValueAt(selectedRow, 1);
+                    String newData = JOptionPane.showInputDialog(null, "Enter new data for " + fieldName + ":", currentData);
+                    if (newData != null) {
+                        model.setValueAt(newData, selectedRow, 1);
+                    }
+                } else {
                     JOptionPane.showMessageDialog(null, "Please select a field to edit.");
                 }
             }
