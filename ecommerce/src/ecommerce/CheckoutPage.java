7474885f9e1c6a072ba705114f46ac5361b00f9c
dhruvbhandari234 <f20210925@pilani.bits-pilani.ac.in>
2024-04-11 15:32:47 +0530

    checkout and cart implementation added

--- ecommerce/src/ecommerce/CheckoutPage.java
+++ ecommerce/src/ecommerce/CheckoutPage.java
@@ -1,5 +1,8 @@
 package ecommerce;
 
+import java.awt.Color;
+import java.sql.SQLException;
+
 //import javax.swing.JComboBox;
 //import javax.swing.JOptionPane;
 //import javax.swing.JTextField;
@@ -8,7 +11,8 @@ import javax.swing.*;
 
 public class CheckoutPage extends JFrame {
     
-	public CheckoutPage() {
+	public CheckoutPage(double cartAmount) {
+		
         String[] paymentOptions = {"Wallet", "Cash on Delivery"};
         JComboBox<String> paymentDropdown = new JComboBox<>(paymentOptions);
 
@@ -23,6 +27,19 @@ public class CheckoutPage extends JFrame {
             System.out.println("Delivery address: " + address);
 
             JOptionPane.showMessageDialog(this, "Order Confirmed", "Confirmation", JOptionPane.INFORMATION_MESSAGE);
+            if(selectedOption == "Wallet") {walletCheckout(cartAmount);}
+            try {
+    			GlobalVariables.statement.executeUpdate(
+    					String.format("Delete from cart where customer_id=%d",GlobalVariables.userID)
+    					);
+    			
+    			
+    		} catch (SQLException e1) {
+    			System.err.println("Failed to connect to the database or execute stored procedure!");
+    			e1.printStackTrace();
+    		}
+            dispose();
+            
         });
 
         JPanel panel = new JPanel();
@@ -40,4 +57,17 @@ public class CheckoutPage extends JFrame {
         add(panel);
         setVisible(true);
     }
+	
+	public void walletCheckout(double cartAmount) {
+		try {
+			GlobalVariables.statement.executeUpdate(
+					String.format("Update user set wallet_amount=wallet_amount- %s where id=%d", cartAmount,GlobalVariables.userID)
+					);
+			
+			
+		} catch (SQLException e1) {
+			System.err.println("Failed to connect to the database or execute stored procedure!");
+			e1.printStackTrace();
+		}
+	}
 }
